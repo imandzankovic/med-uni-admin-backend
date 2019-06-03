5e9963dc8576a5c1067156bffcebe5a6444b36e7
unknown <User@mach1.lan.croz.net>
2019-06-03 15:10:30 +0200

    details

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ require('./server/middleware/appMiddleware')(app);
 app.use('/api', api);
 app.listen(3000)
 
-mongoose.connect('mongodb+srv://imandz:iman@meduni-vneye.mongodb.net/medUniAdmin?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+// mongoose.connect('mongodb+srv://imandz:iman@meduni-vneye.mongodb.net/medUniAdmin?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+//     if (!err)
+//         console.log('Connected to Mongo - MedUniAdmin');
+//         else {
+//             console.log(err)
+//         }
+// })
+
+mongoose.connect('mongodb+srv://imandz:iman@meduni-vneye.mongodb.net/medUni?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err)
-        console.log('Connected to Mongo - MedUniAdmin');
+        console.log('Connected to Mongo - MedUni');
         else {
             console.log(err)
         }
 })
-
-
