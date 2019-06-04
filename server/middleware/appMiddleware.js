8fd5339c6758cc0dce4c8c8655f09080e80a6150
imandzankovic <fruit.10@hotmail.com>
2019-06-04 22:44:56 +0200

    video issues

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,9 +1,16 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 // setup global middleware here
 
 module.exports = function(app) {
-  //app.use(morgan('dev'));
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  app.use(morgan());
+  // app.use(bodyParser.urlencoded({ extended: true }));
+  // app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+
+//app.use(multer({ dest: './uploads/'}))
 };
