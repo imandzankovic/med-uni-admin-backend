b757b609eccf8a183d0733a0db16f366b9c52d1b
imandzankovic <fruit.10@hotmail.com>
2019-08-19 17:23:36 +0200

    again

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
 var express = require('express');
 var app = express();
 var api = require('./server/api/api');
-//var config = require('./server/config/config');
-var mongoose = require('mongoose');
-// var upload = require('express-fileupload');
-// var ResumableUpload = require('node-youtube-resumable-upload');
-// var googleauth = require('google-auth-cli');
-// var google = require('googleapis');
-// let naslov=''
+var mongoose=require('mongoose');
+cors = require('cors');
+app.use(cors());
 
-
-
-
-//require('mongoose').connect(config.db.url);
-
-
+//var config = require('./config/config');
 
 require('./server/middleware/appMiddleware')(app);
-
 app.use('/api', api);
-
-
-
-
-//app.listen(config.port)
-//console.log("Server Started at port 3000");
-
-
-
 app.listen(process.env.PORT || 3000)
 
 
@@ -39,22 +20,15 @@ mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true }, (err) => {
         }
 });
 
+/*mongoose.connect('mongodb://imand:iman.123@ds211168.mlab.com:11168/heroku_v65lvc52', { useNewUrlParser: true }, (err) => {
+    if (!err)
+        console.log('Connected to Mongo - MedUni');
+        else {
+            console.log(err)
+        }
+})
+*/
 
 
-// mongoose.connect('mongodb+srv://imandz:iman@meduni-vneye.mongodb.net/medUniAdmin?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
-//     if (!err)
-//         console.log('Connected to Mongo - MedUniAdmin');
-//         else {
-//             console.log(err)
-//         }
-// })
 
-// mongoose.connect('mongodb+srv://imandz:iman@meduni-vneye.mongodb.net/medUni?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
-//     if (!err)
-//         console.log('Connected to Mongo - MedUni');
-//         else {
-//             console.log(err)
-//         }
-// })
 
-//app.listen(3000)
